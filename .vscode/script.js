73710380af50abf281914b987ab4037c80b8cee4
Ferigoti <ferigoti318@outlook.com>
2025-05-06 10:33:40 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/Avatar2.png")
+    img.setAttribute("alt", "foto de joão de camisa branca olhando pro nada")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/Avatar1.png")
+    img.setAttribute("alt", "foto de joão com cara de bravo dando belezinha")
+  }
+}
